21af3b968bca6a4d1eb2dce1a2b900944fb145ad
Yohan Kim <yohan.kim@innoboost.co.kr>
2018-03-30 16:48:30 +0900

    Add postCSs, and Error Handling

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,10 @@ gulp.task('styles', function() {
   return gulp
     .src('./app/styles/styles.css')
     .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
     .pipe(gulp.dest('./app/temp/styles'))
     .pipe(browserSync.stream());
 });
